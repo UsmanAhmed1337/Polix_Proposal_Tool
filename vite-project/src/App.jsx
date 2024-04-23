e48914b834c6eda5cbbd03fb1541e91608469949
UsmanAhmed1337 <UsmanAhmed1337@gmail.com>
2024-04-23 13:36:30 +0500

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Button variant="contained">Hello world</Button>
+  )
+}
+
+export default App
